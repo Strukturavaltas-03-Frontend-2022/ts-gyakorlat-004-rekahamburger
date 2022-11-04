64afc77f53e41b69d5f023783af64cfd3bb030aa
rekahamburger <rekahamburger@gmail.com>
2022-11-04 13:47:26 +0100

    ts4

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import {TransformerHero} from "./transformer"
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero [] = [
+    new HumanHero (0, "Wonder Woman" ,"female", 25, 50),
+    new HumanHero (1, "Ironman", "male", 50, 500),
+    new HumanHero (2, "bat man", "male", 50, 600 ),
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,11 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    new TransformerHero (3, "Optimus",  2, 4, "autobots"),
+    new TransformerHero (4, "Bloombabas", 2, 4, "autobots"),
+    new TransformerHero (5, "Magathron", 2, 4, "autobots"),
+];
+
+
+
